e442ccd64b2b59721e34321b8138375d7c6e1c59
DesignDing <sd4ue@virginia.edu>
2015-05-30 14:56:49 +0200

    added Artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,5 @@
 class Artist < ActiveRecord::Base
-	
+	has_many :albums
+	has_and_belongs_to_many :songs
+	validates :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20150530114319) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
+    t.text     "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
