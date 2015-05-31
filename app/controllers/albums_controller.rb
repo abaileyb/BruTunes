35d21f4a5894a533352c60163e70ffa232467035
abaileyb <bailey@baileyberro.com>
2015-05-31 17:20:22 +0200

    added songs

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,17 +1,17 @@
 class AlbumsController < ApplicationController
 
 	def create
-    @artist = Artist.find(params[:artist_id])
-    @album = @artist.albums.create(album_params)
-    redirect_to artist_path(@artist)
-  end
+     	@artist = Artist.find(params[:artist_id])
+     	@album = @artist.albums.create(album_params)
+     	redirect_to artist_path(@artist)
+    end
  
- def destroy
-    @artist = Artist.find(params[:artist_id])
-    @album = @artist.albums.find(params[:id])
-    @album.destroy
-    redirect_to artist_path(@artist)
-  end 
+ 	def destroy
+    	@artist = Artist.find(params[:artist_id])
+   		@album = @artist.albums.find(params[:id])
+    	@album.destroy
+    	redirect_to artist_path(@artist)
+  	end 
 
   private
     def album_params
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,7 @@
 class Artist < ActiveRecord::Base
 	has_many :albums
 	has_many :songs, through: :albums
-	# has_and_belongs_to_many :songs
+
+	
 	validates :name, presence: true
 end
